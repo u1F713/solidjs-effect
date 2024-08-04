feabb2f76b5fef5f775a5d3b0e6c2d9df39f3f15
Nyarlathotep <anhedonia@skiff.com>
2024-08-03 20:52:25 -0400

    chore: fmt

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
-import {defineConfig} from 'vite'
-import TSConfigPath from 'vite-tsconfig-paths'
 import {vanillaExtractPlugin} from '@vanilla-extract/vite-plugin'
+import {defineConfig} from 'vite'
 import solid from 'vite-plugin-solid'
+import TSConfigPath from 'vite-tsconfig-paths'
 
 export default defineConfig({
   server: {port: 4443},
